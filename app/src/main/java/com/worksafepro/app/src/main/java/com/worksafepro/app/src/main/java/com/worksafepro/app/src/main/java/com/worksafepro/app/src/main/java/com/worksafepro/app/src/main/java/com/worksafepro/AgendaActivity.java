37221968bdb2804b150f7a885f7254d63c002fd4
Natividade0 <86794017+Natividade0@users.noreply.github.com>
2025-10-28 09:57:06 -0300

    Create AgendaActivity.java

---
+++ app/src/main/java/com/worksafepro/app/src/main/java/com/worksafepro/app/src/main/java/com/worksafepro/app/src/main/java/com/worksafepro/app/src/main/java/com/worksafepro/app/src/main/java/com/worksafepro/AgendaActivity.java
@@ -0,0 +1,12 @@
+package com.worksafepro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AgendaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda);
+    }
+}
